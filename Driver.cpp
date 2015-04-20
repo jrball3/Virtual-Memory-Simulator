1bd24e68076066bd9d9c098a6a051ff5d1463082
Ellington Kirby <ellingtonkirby31@gmail.com>
2015-04-20 18:52:51 -0400

    modified driver to run all three replacement algorithms

--- Driver.cpp
+++ Driver.cpp
@@ -19,7 +19,9 @@ int main(int argc, char* argv[]){
 		//do so using a class, which contains parameters representing frame size
 		//as well as a data structure to store processes
 		//processes should be indexed by process number
-		VirtualMemorySimulator VMS(num_frames, MODE_LRU);
+		VirtualMemorySimulator VMS_LRU(num_frames, MODE_LRU);
+		VirtualMemorySimulator VMS_QUEUE(num_frames, MODE_QUEUE);
+		VirtualMemorySimulator VMS_RANDOM(num_frames, MODE_RANDOM);
 		ifstream infile(filename);
 		if(infile.is_open()){
 			string line, word;
@@ -32,13 +34,17 @@ int main(int argc, char* argv[]){
 					ss >> process_number;
 					ss >> address_space_size;
 					//call start proccess function
-					VMS.start(process_number, address_space_size);
+					VMS_LRU.start(process_number, address_space_size);
+					VMS_QUEUE.start(process_number, address_space_size);
+					VMS_RANDOM.start(process_number, address_space_size);
 				}
 				else if(word == "TERMINATE"){
 					int process_number = 0;
 					ss >> process_number;
 					//call terminate process function
-					VMS.terminate(process_number);
+					VMS_LRU.terminate(process_number);
+					VMS_QUEUE.terminate(process_number);
+					VMS_RANDOM.terminate(process_number);
 				}
 				else{//REFERENCE
 					int process_number = 0;
@@ -46,7 +52,7 @@ int main(int argc, char* argv[]){
 					ss >> process_number;
 					ss >> page_number;
 					//call reference function
-					int result = VMS.reference(process_number, page_number);
+					int result = VMS_LRU.reference(process_number, page_number);
 					if(result == PAGE_FAULT){
 						cout << "Encountered a page fault when attempting to add a reference from process id " << process_number << " to page number " << page_number << endl;
 					}
@@ -56,9 +62,10 @@ int main(int argc, char* argv[]){
 					else if(result == PAGE_RESIDENT){
 						cout << "Page number " << page_number << " is already resident to process id " << process_number << endl;
 					}
-					else{
-						continue;	
-					}
+					
+					int result2 = VMS_QUEUE.reference(process_number, page_number);	
+					int result3 = VMS_RANDOM.reference(process_number, page_number);	
+						
 				}	
 			}
 		}
@@ -66,7 +73,9 @@ int main(int argc, char* argv[]){
 
 
 		// Print fault counts
-		cout << "QUEUE Fault count:" << VMS.getFaultCount() << endl;
+		cout << "LRU Fault count:" << VMS_LRU.getFaultCount() << endl;
+		cout << "QUEUE Fault count:" << VMS_QUEUE.getFaultCount() << endl;
+		cout << "RANDOM Fault count:" << VMS_RANDOM.getFaultCount() << endl;
 	}
 	return 0;
 }
